8d8948205d4a17353df31f035cbbb5693637a48b
Bananamilk452 <bananamilk452@gmail.com>
2024-07-15 04:08:05 +0900

    커맨드 타입 추가, deploy async

--- src/classes/BotSlashCommand.ts
+++ src/classes/BotSlashCommand.ts
@@ -1,14 +1,20 @@
 import type {
   SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+  SlashCommandOptionsOnlyBuilder,
   ChatInputCommandInteraction,
 } from "discord.js";
 
+type SlashCommand =
+  | SlashCommandBuilder
+  | SlashCommandSubcommandsOnlyBuilder
+  | SlashCommandOptionsOnlyBuilder;
 export default class BotSlashCommand {
-  public command: SlashCommandBuilder;
+  public command: SlashCommand;
   public execute: (interaction: ChatInputCommandInteraction) => any;
 
   constructor(
-    command: SlashCommandBuilder,
+    command: SlashCommand,
     execute: (interaction: ChatInputCommandInteraction) => any
   ) {
     this.command = command;
--- src/deploy-command.ts
+++ src/deploy-command.ts
@@ -22,36 +22,38 @@ const commandsInJson = commands.map((command) => {
 
 const rest = new REST({ version: "10" }).setToken(token);
 
-try {
-  const isProduction = process.env.NODE_ENV === "production";
-  const guildId = process.env.TO_REGISTER_GUILD;
+async () => {
+  try {
+    const isProduction = process.env.NODE_ENV === "production";
+    const guildId = process.env.TO_REGISTER_GUILD;
 
-  if (!isProduction) {
-    logger.warn("Using guild commands deployment in development mode.");
-    if (!guildId) {
-      logger.error("Guild ID not found in environment variables.");
-      process.exit(1);
+    if (!isProduction) {
+      logger.warn("Using guild commands deployment in development mode.");
+      if (!guildId) {
+        logger.error("Guild ID not found in environment variables.");
+        process.exit(1);
+      }
     }
-  }
 
-  logger.info("Started refreshing application (/) commands.");
+    logger.info("Started refreshing application (/) commands.");
 
-  if (!isProduction && guildId) {
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commandsInJson,
+    if (!isProduction && guildId) {
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: commandsInJson,
+      });
+    } else {
+      await rest.put(Routes.applicationCommands(clientId), {
+        body: commandsInJson,
+      });
+    }
+
+    logger.info("Successfully reloaded application (/) commands.");
+
+    logger.info(`Deployed commands:`);
+    commands.forEach((command) => {
+      logger.info(`- ${command.command.name}`);
     });
-  } else {
-    await rest.put(Routes.applicationCommands(clientId), {
-      body: commandsInJson,
-    });
+  } catch (error) {
+    logger.error(error);
   }
-
-  logger.info("Successfully reloaded application (/) commands.");
-
-  logger.info(`Deployed commands:`);
-  commands.forEach((command) => {
-    logger.info(`- ${command.command.name}`);
-  });
-} catch (error) {
-  logger.error(error);
-}
+};
