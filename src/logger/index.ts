86fa15ac8ea450305dbcca4ec3d108e30930e56c
Bananamilk452 <bananamilk452@gmail.com>
2024-07-20 00:12:34 +0900

    프로덕션에서도 로깅 켜기

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -26,19 +26,10 @@ const logger = createLogger({
       filename: `${dateTimeString}.log`,
       dirname: "logs",
     }),
-  ],
-});
-
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
     new transports.Console({
       format: format.combine(format.colorize(), myFormat),
-    })
-  );
-}
+    }),
+  ],
+});
 
 export default logger;
